f411ace7a2d8f78ee5776972ffe70217db390a8f
Sungjun Lee <piero96@naver.com>
2018-09-27 23:17:52 +0900

    Hanoi2

--- Chap2/Hanoi.c
+++ Chap2/Hanoi.c
@@ -7,9 +7,28 @@
 
 #include <stdio.h>
 
+void TowersOfHanoi(int n, char fromH, char toH, char auxH)
+{
+	if(n==1)
+	{
+		printf("Disk#%d from %c to %c\n", n, fromH, toH);
+		return;
+	}
+
+	TowersOfHanoi(n-1, fromH, auxH, toH);
+
+	printf("Disk#%d from %c to %c\n", n, fromH, toH);
+
+	TowersOfHanoi(n-1, auxH, toH, fromH);
+}
+
 int main(void)
 {
-	printf("This is a program for resolving Hanoi Top issue.\n");
+	int num;
+	printf("This is a program for resolving Hanoi Top issue!!!\n");
+
+	num = 3;
+	TowersOfHanoi(num, 'A', 'C', 'B');
 
 	return 0;
 }
