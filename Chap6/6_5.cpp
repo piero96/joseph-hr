266f6cf67b9f3eb8153277e5f963154e655a5bd8
piero96 <piero96@naver.com>
2023-07-15 22:22:02 +0900

    6.5 PICNIC right recursion

---
+++ Chap6/6_5.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int n;
+bool areFriends[10][10];
+
+int countPairings(bool taken[10]){
+    int firstFree = -1;
+    for(int i=0; i<n; ++i){
+        if(!taken[i]){
+            firstFree = i;
+            break;
+        }
+    }
+
+    if(firstFree == -1){
+        cout << "done" << endl; 
+    }
+    int ret = 0;
+
+    for(int pairWith = firstFree+1; pairWith<n; ++pairWith){
+        if(!taken[pairWith] && areFriends[firstFree][pairWith]){
+            taken[firstFree] = taken[pairWith] = true;
+            cout << firstFree << "," << pairWith << endl;
+            ret += countPairings(taken);
+            taken[firstFree] = taken[pairWith] = false;
+        }
+    }
+    return ret;
+}
+
+int main(void){
+    n = 4;
+    bool tk[10] = {false};
+    areFriends[1][4]=true;
+    areFriends[1][2]=true;
+    areFriends[2][3]=true;
+    areFriends[3][4]=true;
+    areFriends[2][3]=true;
+    areFriends[1][3]=true;
+    cout << countPairings(tk) << endl;
+    return 0;
+}
