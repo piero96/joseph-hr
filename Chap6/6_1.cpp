4439aa84cb0bc409442cea96ec49051be69c63f2
piero96 <piero96@naver.com>
2023-07-15 09:22:45 +0900

    6.1 재귀함수

---
+++ Chap6/6_1.cpp
@@ -0,0 +1,12 @@
+int sum(int n){
+    int ret = 0;
+    for(int i=1; i<=n; ++i){
+        ret += i;
+    }
+    return ret;
+}
+
+int recursiveSum(int n){
+    if(n==1) return 1;
+    return n + recursiveSum(n-1);
+}
