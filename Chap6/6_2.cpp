dec195b57113ccd4d3cb578c296fb6727c286cb4
piero96 <piero96@naver.com>
2023-07-15 10:02:23 +0900

    6.2 update

--- Chap6/6_2.cpp
+++ Chap6/6_2.cpp
@@ -1,10 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void printPicked(vector<int>& picked){
+    vector<int>::iterator it;
+    //cout << "Test" << endl;
+
+    for(it=picked.begin(); it!=picked.end(); it++){
+        cout << *it << " ";
+    }
+    cout << endl;
+}
 void pick(int n, vector<int>& picked, int toPick){
     if(toPick==0) { printPicked(picked); return; }
-    int smallest = picked.empty() ? 0:picked.back() + 1;
+    int smallest = picked.empty() ? 1:picked.back() + 1;
 
     for(int next=smallest; next<n; ++next){
         picked.push_back(next);
         pick(n, picked, toPick-1);
-        picked.pop_back(next);
+        picked.pop_back();
     }
+}
+
+int main(void){
+    vector<int> picked;
+    int n;
+    n = 7;
+    pick(n,picked,3);
+
+    return 0;
 }
